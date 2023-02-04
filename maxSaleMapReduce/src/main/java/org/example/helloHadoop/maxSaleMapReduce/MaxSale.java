77ac2e824331df8ba679e4321deae77b9b666d34
xunpeng liu <l378923493@outlook.com>
2023-02-04 21:33:16 +0800

    集群化改造，加入集群运行配置文件

--- maxSaleMapReduce/src/main/java/org/example/helloHadoop/maxSaleMapReduce/MaxSale.java
+++ maxSaleMapReduce/src/main/java/org/example/helloHadoop/maxSaleMapReduce/MaxSale.java
@@ -1,5 +1,6 @@
 package org.example.helloHadoop.maxSaleMapReduce;
 
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -7,14 +8,28 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 /**
  * 启动程序
  */
-public class MaxSale {
+public class MaxSale extends Configured implements Tool {
 
     public static void main(String[] args) throws Exception {
-        Job job = Job.getInstance();
+        int exitCode = ToolRunner.run(new MaxSale(), args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public int run(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("args error");
+            ToolRunner.printGenericCommandUsage(System.err);
+            return -1;
+        }
+
+        Job job = Job.getInstance(getConf());
         job.setJarByClass(MaxSale.class);
         job.setJobName("MaxSale");
 
@@ -31,6 +46,6 @@ public class MaxSale {
         //设置Reduce任务数
         job.setNumReduceTasks(1);
 
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+        return job.waitForCompletion(true) ? 0 : 1;
     }
 }
