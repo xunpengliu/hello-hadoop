1474dd85a4d0b73b1b3603c1e4915a243d62a9da
xunpeng liu <l378923493@outlook.com>
2023-02-06 11:04:41 +0800

    任务新增追踪日志

--- maxSaleMapReduce/src/main/java/org/example/helloHadoop/maxSaleMapReduce/MaxSaleMapper.java
+++ maxSaleMapReduce/src/main/java/org/example/helloHadoop/maxSaleMapReduce/MaxSaleMapper.java
@@ -4,17 +4,39 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.example.helloHadoop.common.entity.RuntimeInfoEntity;
 import org.example.helloHadoop.common.entity.SaleDataEntity;
 import org.example.helloHadoop.common.parse.SaleDataParse;
 import org.example.helloHadoop.common.parse.TextParse;
+import org.example.helloHadoop.common.tool.RuntimeInfoTool;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 /**
  * 解析输入的文本数据
  */
 public class MaxSaleMapper extends Mapper<LongWritable, Text, LongWritable, IntWritable> {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     protected TextParse<SaleDataEntity> saleDataParse = new SaleDataParse();
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        super.setup(context);
+        RuntimeInfoEntity runInfo = RuntimeInfoTool.getRunInfo();
+        String startTime = null;
+        if (runInfo.getStartTime() != null) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            startTime = sdf.format(runInfo.getStartTime());
+        }
+        logger.info("mapper task,ip->{},startTime->{},mainClass->{},pid->{}",
+                runInfo.getIpv4(),
+                startTime,
+                runInfo.getMainClass(),
+                runInfo.getPid());
+    }
 
     @Override
     protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
--- maxSaleMapReduce/src/main/java/org/example/helloHadoop/maxSaleMapReduce/MaxSaleReducer.java
+++ maxSaleMapReduce/src/main/java/org/example/helloHadoop/maxSaleMapReduce/MaxSaleReducer.java
@@ -3,13 +3,36 @@ package org.example.helloHadoop.maxSaleMapReduce;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.example.helloHadoop.common.entity.RuntimeInfoEntity;
+import org.example.helloHadoop.common.tool.RuntimeInfoTool;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 /**
  * 查找每一个用户的最大销售值
  */
 public class MaxSaleReducer extends Reducer<LongWritable, IntWritable, LongWritable, IntWritable> {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        super.setup(context);
+        RuntimeInfoEntity runInfo = RuntimeInfoTool.getRunInfo();
+        String startTime = null;
+        if (runInfo.getStartTime() != null) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            startTime = sdf.format(runInfo.getStartTime());
+        }
+        logger.info("reducer task,ip->{},startTime->{},mainClass->{},pid->{}",
+                runInfo.getIpv4(),
+                startTime,
+                runInfo.getMainClass(),
+                runInfo.getPid());
+    }
+
     @Override
     protected void reduce(LongWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         int max = 0;
